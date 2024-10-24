ce93fe7faffa7c79e699cc4ae84c12fbd60d8f47
JeriSan <jerisanwork@gmail.com>
2024-10-24 14:01:57 -0500

    modificaciones de los div de confianza

---
+++ javascript/Home/home.js
@@ -0,0 +1,56 @@
+function cambiarImagenSilver(hover) {
+    const logoImage = document.getElementById('silver-image');
+    if (hover) {
+        logoImage.src = 'recursos/Home/Confian/Silver/Silver_2.svg';
+    } else {
+        logoImage.src = 'recursos/Home/Confian/Silver/Silver_1.svg';
+    }
+}
+function cambiarImagenIuva(hover) {
+    const logoImage = document.getElementById('iuva-image');
+    if (hover) {
+        logoImage.src = 'recursos/Home/Confian/Iuva/IUVA_2.svg';
+    } else {
+        logoImage.src = 'recursos/Home/Confian/Iuva/IUVA_1.svg';
+    }
+}
+function cambiarImagen50Plan(hover) {
+    const logoImage = document.getElementById('50plan-image');
+    if (hover) {
+        logoImage.src = 'recursos/Home/Confian/50plan/50_2.svg';
+    } else {
+        logoImage.src = 'recursos/Home/Confian/50plan/50_1.svg';
+    }
+}
+function cambiarImagenHouseClub(hover) {
+    const logoImage = document.getElementById('houseClub-image');
+    if (hover) {
+        logoImage.src = 'recursos/Home/Confian/HouseClub/House_Club_2.svg';
+    } else {
+        logoImage.src = 'recursos/Home/Confian/HouseClub/House_Club_1.svg';
+    }
+}
+function cambiarImagenToyCars(hover) {
+    const logoImage = document.getElementById('toyCars-image');
+    if (hover) {
+        logoImage.src = 'recursos/Home/Confian/ToyCars/Toy_Cars_2.svg';
+    } else {
+        logoImage.src = 'recursos/Home/Confian/ToyCars/Toy_Cars_1.svg';
+    }
+}
+function cambiarImagenMumma(hover) {
+    const logoImage = document.getElementById('mumma-image');
+    if (hover) {
+        logoImage.src = 'recursos/Home/Confian/Mumma/Muma_Net_2.svg';
+    } else {
+        logoImage.src = 'recursos/Home/Confian/Mumma/Muma_Net_1.svg';
+    }
+}
+function cambiarImagenVendenos(hover) {
+    const logoImage = document.getElementById('vendenos-image');
+    if (hover) {
+        logoImage.src = 'recursos/Home/Confian/Vendenos/Vendenos_2.svg';
+    } else {
+        logoImage.src = 'recursos/Home/Confian/Vendenos/Vendenos_1.svg';
+    }
+}
